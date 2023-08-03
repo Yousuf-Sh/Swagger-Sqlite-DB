c9afaa544e2a5f85d6cfa55ffd790fb8c1d9e97a
M.Yousuf <56998778+Yousuf-Sh@users.noreply.github.com>
2023-08-03 17:09:39 +0500

    Converted to GORM

--- go.mod
+++ go.mod
@@ -21,6 +21,8 @@ require (
 	github.com/go-openapi/analysis v0.21.4 // indirect
 	github.com/go-openapi/jsonpointer v0.19.5 // indirect
 	github.com/go-openapi/jsonreference v0.20.0 // indirect
+	github.com/jinzhu/inflection v1.0.0 // indirect
+	github.com/jinzhu/now v1.1.5 // indirect
 	github.com/josharian/intern v1.0.0 // indirect
 	github.com/mailru/easyjson v0.7.7 // indirect
 	github.com/mitchellh/mapstructure v1.5.0 // indirect
@@ -28,4 +30,6 @@ require (
 	go.mongodb.org/mongo-driver v1.11.3 // indirect
 	golang.org/x/sys v0.10.0 // indirect
 	gopkg.in/yaml.v3 v3.0.1 // indirect
+	gorm.io/driver/sqlite v1.5.2 // indirect
+	gorm.io/gorm v1.25.2 // indirect
 )
--- restapi/conDB.go
+++ restapi/conDB.go
@@ -1,19 +1,35 @@
 package restapi
 
 import (
-	"database/sql"
-	"log"
-
-	_ "github.com/mattn/go-sqlite3"
+	"gorm.io/driver/sqlite"
+	"gorm.io/gorm"
 )
 
-var DB *sql.DB
+// Define the User model for GORM
+type dbUser struct {
+	gorm.Model
+	Name  *string `gorm:"column:name"`
+	Email *string `gorm:"column:email"`
+}
 
-func InitDB() {
-	var err error
-	DB, err = sql.Open("sqlite3", "../userDB.db")
+func (dbUser) TableName() string {
+	return "db_users"
+}
+
+var DB *gorm.DB
+
+func InitDB() (*gorm.DB, error) {
+	db, err := gorm.Open(sqlite.Open("../userDB.db"), &gorm.Config{})
 	if err != nil {
-		log.Fatalf("failed to open database: %v", err)
+		return nil, err
 	}
 
+	// Auto-migrate the User model to create the "users" table if it doesn't exist
+	err = db.AutoMigrate(&dbUser{})
+	if err != nil {
+		return nil, err
+	}
+
+	DB = db
+	return db, nil
 }
